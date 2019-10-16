48a49bb65ffef0937158a0e73e2865a41db502ed
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-16 14:14:14 +0100

    Test reporting bike broken failing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,13 @@ describe Bike do
   # So that I can use a good bike,
   # I'd like to see if a bike is working
   it { is_expected.to respond_to :working? }
+
+  # As a member of the public,
+  # So that I reduce the chance of getting a broken bike in future,
+  # I'd like to report a bike as broken when I return it.
+  it "can be reported broken" do
+    subject.report_broken
+    # let's use one of RSpec's predicate matchers
+    expect(subject).to be_broken
+  end
 end
