664d78f025117e006ebf33382269077af12a0803
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-16 10:57:29 +0100

    Test access to bike failing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,8 @@
 require "bike"
 
 describe Bike do
+  # As a person,
+  # So that I can use a good bike,
+  # I'd like to see if a bike is working
   it { is_expected.to respond_to :working? }
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,26 @@
 require "docking_station"
 
 describe DockingStation do
+  # As a person,
+  # So that I can use a bike,
+  # I'd like a docking station to release a bike.
   it { is_expected.to respond_to :release_bike }
 
+  # As a person,
+  # So that I can use a good bike,
+  # I'd like to see if a bike is working
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
 
+  # As a member of the public
+  # So I can return bikes I've hired
+  # I want to dock my bike at the docking station
   it { is_expected.to respond_to(:dock).with(1).argument }
+
+  # As a member of the public
+  # So I can decide whether to use the docking station
+  # I want to see a bike that has been docked
+  it { is_expected.to respond_to(:bike) }
 end
