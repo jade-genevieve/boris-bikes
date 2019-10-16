43c8b33b68152886692f3404a444d6e2b5492d28
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-16 16:08:42 +0100

    Test garage capacity failing

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,4 +1,9 @@
 require "garage"
 
 describe Garage do
+  describe "initialization" do
+    it "has capacity for broken bikes" do
+      it { is_expected.to respond_to :capacity }
+    end
+  end
 end
