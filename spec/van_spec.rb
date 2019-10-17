6d712dbe52c85304c37f9bc87ad745931293ca32
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-17 10:05:42 +0100

    Test new class Van failing

---
+++ spec/van_spec.rb
@@ -0,0 +1,16 @@
+describe Van do
+  it { is_expected.to be_a_kind_of(Van) }
+
+  # describe "initialization" do
+  #   it { is_expected.to respond_to :capacity }
+  # end
+
+  # it "can receive an array of bikes" do
+  #   van = double(:van, bikes: [1, 2, 3])
+  #   allow(van).to receive(:deliver_to_garage) do |garage|
+  #     garage.bikes = van.bikes
+  #   end
+  #   van.deliver_to_garage(subject)
+  #   expect(subject.bikes).to eq([1, 2, 3])
+  # end
+end
