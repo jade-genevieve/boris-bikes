44bc5a922128a8c0423852d0eecaf93e9289546a
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-17 10:25:21 +0100

    Test van stores collected vans failing

--- lib/van.rb
+++ lib/van.rb
@@ -1,9 +1,10 @@
 class Van
   DEFAULT_CAPACITY = 6
 
-  attr_reader :capacity
+  attr_reader :capacity, :bike
 
   def initialize
+    @bikes = []
     @capacity = DEFAULT_CAPACITY
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -7,12 +7,20 @@ describe Van do
     it { is_expected.to respond_to :capacity }
   end
 
-  # it "can receive an array of bikes" do
-  #   van = double(:van, bikes: [1, 2, 3])
-  #   allow(van).to receive(:deliver_to_garage) do |garage|
-  #     garage.bikes = van.bikes
+  describe "#collect_bikes" do
+    it "should store broken bikes for transporting" do
+      broken_bikes = [:bike1, :bike2, :bike3]
+      mock_dock = double(:docking_station, collect_bikes: broken_bikes)
+      expect(subject.collect_bikes).to eq([:bike1, :bike2, :bike3])
+    end
+  end
+
+  #   it "can receive an array of bikes" do
+  #     van = double(:van, bikes: [1, 2, 3])
+  #     allow(van).to receive(:deliver_to_garage) do |garage|
+  #       garage.bikes = van.bikes
+  #     end
+  #     van.deliver_to_garage(subject)
+  #     expect(subject.bikes).to eq([1, 2, 3])
   #   end
-  #   van.deliver_to_garage(subject)
-  #   expect(subject.bikes).to eq([1, 2, 3])
-  # end
 end
