942a4d45541b98d62cb499e58144f1a71eae5fab
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-17 10:09:10 +0100

    Test van for capacity failing

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,9 +3,9 @@ require "van"
 describe Van do
   it { is_expected.to be_an_instance_of(Van) }
 
-  # describe "initialization" do
-  #   it { is_expected.to respond_to :capacity }
-  # end
+  describe "initialization" do
+    it { is_expected.to respond_to :capacity }
+  end
 
   # it "can receive an array of bikes" do
   #   van = double(:van, bikes: [1, 2, 3])
